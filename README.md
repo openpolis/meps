6a53479790f65a1d8af92ac6ce5e10a01e4ff724
guglielmo <guglielmo.celata@gmail.com>
2017-01-06 23:06:25 +0100

    scraper implemented

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-This is a scraper that runs on [Morph](https://morph.io). To get started [see the documentation](https://morph.io/documentation)
+This is a scraper that runs on [Morph](https://morph.io), used to parse information for
+the members of european parliaments out of the official web site.
+
--- scraper.py
+++ scraper.py
@@ -1,24 +1,126 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki
+import lxml.html
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+euparl_url = "http://www.europarl.europa.eu"
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+# read list of meps
+print("Reading list from EU Parliament web site")
+html = scraperwiki.scrape(
+    "{0}/meps/en/full-list.html?filter=all&leg=".format(euparl_url)
+)
+
+root = lxml.html.fromstring(html)
+for div in root.cssselect("div[class='zone_info_mep ']"):
+    mep_link = div.cssselect("li[class='mep_name'] a")[0]
+    mep_name = mep_link.text
+    mep_url = mep_link.attrib['href']
+
+    mep_id = int(mep_url.split('/')[3])
+
+    print(u"{0} => {1}".format(mep_name, mep_url))
+    mep_html = scraperwiki.scrape(
+        "{0}{1}".format(euparl_url, mep_url)
+    )
+    mep_root = lxml.html.fromstring(mep_html)
+
+    mep_group = mep_root.cssselect(
+        "#zone_before_content_global li.group"
+    )[0].text.strip()
+
+    try:
+        mep_nationality = mep_root.cssselect(
+            "#zone_before_content_global li.nationality"
+        )[0].text.strip()
+    except IndexError:
+        mep_nationality = None
+
+    try:
+        mep_party = mep_root.cssselect(
+            "#zone_before_content_global li.nationality span.name_pol_group"
+        )[0].text.strip()
+    except IndexError:
+        mep_party = None
+
+    try:
+        (mep_birth_date, mep_birth_place) = mep_root.cssselect(
+            "#zone_before_content_global li.nationality + span.more_info"
+        )[0].text.strip().split(',', 1)
+        mep_birth_date = mep_birth_date.replace('Date of birth: ', '')
+        mep_birth_place = mep_birth_place.strip()
+    except ValueError:
+        mep_birth_date = None
+        mep_birth_place = None
+
+    try:
+        image = mep_root.cssselect(
+            "#zone_before_content_global > div > img"
+        )[0].attrib['src']
+    except IndexError:
+        image = None
+
+    try:
+        feed = mep_root.cssselect(
+            "#content_right > div.widget.socials > ul > li.rss > a"
+        )[0].attrib['href']
+    except IndexError:
+        feed = None
+
+    try:
+        email = mep_root.cssselect("#email-0")[0].attrib['href'].replace(
+            '[dot]', '.').replace('[at]', '@').replace("mailto:", "")[::-1]
+    except IndexError:
+        email = None
+
+    try:
+        website = mep_root.cssselect(
+            "#content_right ul.link_collection_noborder "
+            "li a.link_website"
+        )[0].attrib['href']
+    except IndexError:
+        website = None
+
+    try:
+        facebook = mep_root.cssselect(
+            "#content_right ul.link_collection_noborder "
+            "li a.link_fb"
+        )[0].attrib['href']
+    except IndexError:
+        facebook = None
+
+    try:
+        twitter = mep_root.cssselect(
+            "#content_right ul.link_collection_noborder "
+            "li a.link_twitt"
+        )[0].attrib['href']
+    except IndexError:
+        twitter = None
+
+    try:
+        youtube = mep_root.cssselect(
+            "#content_right ul.link_collection_noborder "
+            "li a.link_youtube"
+        )[0].attrib['href']
+    except IndexError:
+        youtube = None
+
+
+    # write record out to the sqlite database
+    scraperwiki.sqlite.save(
+        unique_keys=['id'],
+        data={
+            "id": mep_id,
+            "name": mep_name,
+            "eu_url": "{0}{1}".format(euparl_url, mep_url),
+            "group": mep_group,
+            "nationality": mep_nationality,
+            "party": mep_party,
+            "birth_date": mep_birth_date,
+            "birth_place": mep_birth_place,
+            "image_url": "{0}{1}".format(euparl_url, image),
+            "email": email,
+            "website": website,
+            "facebook": facebook,
+            "twitter": twitter,
+            "youtube": youtube
+        }
+    )
